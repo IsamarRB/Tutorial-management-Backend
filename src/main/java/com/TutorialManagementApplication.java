49ca156f002c7dae19c1a0d01b489288f9823a37
IsamarRB <romerobellorini@gmail.com>
2024-07-09 12:03:54 +0200

    SpringBoot

---
+++ src/main/java/com/TutorialManagementApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialManagementApplication.class, args);
+	}
+
+}
