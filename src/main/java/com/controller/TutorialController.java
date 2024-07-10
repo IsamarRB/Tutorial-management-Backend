0b537d223594f7e8c3d5a4a898607847367f624f
IsamarRB <romerobellorini@gmail.com>
2024-07-10 13:11:52 +0200

    Create Tutorial

--- src/main/java/com/controller/TutorialController.java
+++ src/main/java/com/controller/TutorialController.java
@@ -1,4 +1,5 @@
 package com.controller;
+
 import com.model.Tutorial;
 import com.services.TutorialServices;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/")
 @CrossOrigin(origins = "*")
-
 public class TutorialController {
-    @Autowired
-    TutorialServices tutorialService;
 
+    @Autowired
+    private TutorialServices tutorialServices;
+
+    @PostMapping(path = "/Tutorial")
+    public Tutorial createTutorial(@RequestBody Tutorial newTutorial) {
+        return tutorialServices.createTutorial(newTutorial);
+    }
 }
