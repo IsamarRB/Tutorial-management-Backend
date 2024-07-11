23a33acc0952e37a5c70e9c8ec47704c843b00c4
IsamarRB <romerobellorini@gmail.com>
2024-07-11 12:27:18 +0200

    Changes

--- src/test/java/com/TutorialManagementApplicationTests.java
+++ src/test/java/com/TutorialManagementApplicationTests.java
@@ -1,13 +1,15 @@
-package com;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+package com.;
 
 @SpringBootTest
-class TutorialManagementApplicationTests {
+@ExtendWith(SpringExtension.class)
+public class TutorialManagementApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
-
 }
