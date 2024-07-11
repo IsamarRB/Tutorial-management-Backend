2e13bb00578d983a43523daa76a9b04bd29da52d
Raunier24 <peplisoft@gmail.com>
2024-07-11 10:18:27 +0200

    GetMapping Created

--- src/main/java/com/controller/TutorialController.java
+++ src/main/java/com/controller/TutorialController.java
@@ -1,8 +1,18 @@
 package com.controller;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.model.Tutorial;
 import com.services.TutorialServices;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+
+
 
 @RestController
 @RequestMapping("/")
@@ -15,5 +25,16 @@ public class TutorialController {
     @DeleteMapping(path = "/Tutorial/{id}")
     public String deleteTutorial(@PathVariable int id) {
         return tutorialService.deleteTutorial(id);
+}
+
+    @GetMapping(path = "/Tutorial")
+    public ArrayList<Tutorial> getAllTutorial() {
+        return tutorialService.getAllTutorial();
     }
+    
+    @GetMapping(path = "/wellcome")
+    public String sayHello() {
+        return "Wellcome to our tutorial";
+    }
+    
 }
